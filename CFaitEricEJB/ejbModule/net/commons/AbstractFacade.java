ad8e3dbf3aa4fd203aa54b0b5578d3d4e4d41e60
eric.gabriac <eric.gabriac@192.168.59.81>
2016-05-21 18:07:40 +0200

    oubli

--- CFaitEricEJB/ejbModule/net/commons/AbstractFacade.java
+++ CFaitEricEJB/ejbModule/net/commons/AbstractFacade.java
@@ -3,25 +3,24 @@ package net.commons;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import lombok.Getter;
 import net.entetrs.commons.jpa.AbstractDaoEntrepriseEdition;
 
 /**
- * offre la capacité de retourner l'entity manager. Cette classe doit être hérité par les façades
- * concrêtes annotées par Stateless.
+ * offre la capacité de retourner l'entity manager. Cette classe doit être
+ * hérité par les façades concrêtes annotées par Stateless.
  * 
  * @author CDT RBN
  *
  * @param <T>
  */
-
-public abstract class AbstractFacade<T> extends AbstractDaoEntrepriseEdition<T>
-{
+@Getter
+public abstract class AbstractFacade<T> extends AbstractDaoEntrepriseEdition<T> {
 	@PersistenceContext
 	private EntityManager em;
 
 	@Override
-	protected EntityManager getEntityManager()
-	{
+	protected EntityManager getEntityManager() {
 		return em;
 	}
 
